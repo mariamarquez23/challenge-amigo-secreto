fb72580b54de48202eb27fdcfad8630d3ff2a291
María Márquez Rodriguez <mariamarod19@gmail.com>
2025-01-17 18:15:26 -0500

    actualizando

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 let listaNombres = [];
 
 function agregarAmigos() {
-    
+    const nombre = document.getElementById("amigo").value;
+    if (nombre != "") {
+        listaNombres.push(nombre);
+        actualizarAmigos();
+    } else {
+        alert("Ingrese un nombre válido.");
+    }
+    document.getElementById("amigo").value = "";
 }
 
 function actualizarAmigos() {
-    
+    const nombres = document.querySelector("#listaAmigos");
+    nombres.innerHTML = "";
+    for (let i = 0; i < listaNombres.length; i++) {
+        const nombre = listaNombres[i];
+        const li = document.createElement("li");
+        li.textContent = nombre;
+        nombres.appendChild(li);
+    }
 }
 
 function sortearAmigos() {
